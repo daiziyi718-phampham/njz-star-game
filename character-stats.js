8860e5a2be6b8c2ae5d85b20a70044ceb51d24d7
DZY <dzy@DZYdeMacBook-Air.local>
2026-08-16 21:45:09 +0800

    Web部署前完整版

---
+++ character-stats.js
@@ -0,0 +1,62 @@
+window.CHARACTER_STATS = [
+  {
+    id: 1,
+    skillDescription: "额外拥有1条生命",
+    speedMultiplier: 1,
+    livesBonus: 1,
+    shieldCharges: 0,
+    laserCooldownMultiplier: 1,
+    laserKillGlobalStunFrames: 0,
+    starMagnetRange: 0,
+    laserRangeMultiplier: 1,
+    respawnInvincibilityMultiplier: 1
+  },
+  {
+    id: 2,
+    skillDescription: "激光冷却时间缩短40%",
+    speedMultiplier: 1,
+    livesBonus: 0,
+    shieldCharges: 0,
+    laserCooldownMultiplier: 0.6,
+    laserKillGlobalStunFrames: 0,
+    starMagnetRange: 0,
+    laserRangeMultiplier: 1,
+    respawnInvincibilityMultiplier: 1
+  },
+  {
+    id: 3,
+    skillDescription: "自动收集周围8格内可达的星星",
+    speedMultiplier: 1,
+    livesBonus: 0,
+    shieldCharges: 0,
+    laserCooldownMultiplier: 1,
+    laserKillGlobalStunFrames: 0,
+    starMagnetRange: 1,
+    laserRangeMultiplier: 1,
+    respawnInvincibilityMultiplier: 1
+  },
+  {
+    id: 4,
+    skillDescription: "移动速度提升30%",
+    speedMultiplier: 1.2,
+    livesBonus: 0,
+    shieldCharges: 0,
+    laserCooldownMultiplier: 1,
+    laserKillGlobalStunFrames: 0,
+    starMagnetRange: 0,
+    laserRangeMultiplier: 1,
+    respawnInvincibilityMultiplier: 1
+  },
+  {
+    id: 5,
+    skillDescription: "激光击杀敌人后，全场敌人冻结2秒",
+    speedMultiplier: 1,
+    livesBonus: 0,
+    shieldCharges: 0,
+    laserCooldownMultiplier: 1,
+    laserKillGlobalStunFrames: 120,
+    starMagnetRange: 0,
+    laserRangeMultiplier: 1,
+    respawnInvincibilityMultiplier: 1
+  }
+];
